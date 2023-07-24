7bf02e9fea266764f7d65854cc0e19ecfe424180
apinanyogaratnam <apinan@ontopical.com>
2023-07-23 23:31:31 -0400

    refactor: error handling for non existent key

--- main.go
+++ main.go
@@ -21,6 +21,7 @@ func main() {
 
 	if err != nil {
 		log.Println("Failed to run cspell:", err)
+		return
 	}
 
 	// Parse output
@@ -34,10 +35,15 @@ func main() {
 	if _, err := os.Stat(settingsPath); err == nil {
 		settingsFile, err := ioutil.ReadFile(settingsPath)
 		if err != nil {
-			log.Fatal("Failed to read settings:", err)
+			log.Println("Failed to read settings:", err)
+			return
 		}
 		if err := json.Unmarshal(settingsFile, &settings); err != nil {
-			log.Fatal(err)
+			log.Println(err)
+			return
+		}
+		if settings.Words == nil {
+			log.Println("Warning: 'cSpell.words' key not found in settings file. A new key will be added.")
 		}
 		settings.Words = append(settings.Words, unknownWords...)
 	} else {
@@ -50,10 +56,12 @@ func main() {
 	// Write back to settings.json
 	settingsBytes, err := json.MarshalIndent(settings, "", "  ")
 	if err != nil {
-		log.Fatalln("Failed to marshal settings:", err)
+		log.Println("Failed to marshal settings:", err)
+		return
 	}
 	if err := ioutil.WriteFile(settingsPath, settingsBytes, 0644); err != nil {
-		log.Fatalln("Failed to write settings:", err)
+		log.Println("Failed to write settings:", err)
+		return
 	}
 
 	fmt.Println("Done!")
